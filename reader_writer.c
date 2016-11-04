a8644292e65e8676093305c4e469c29eb08bad71
Frank Dattalo <dattalo.2@osu.edu>
2016-11-03 20:10:20 -0400

    added solution that prefers writers

--- reader_writer.c
+++ reader_writer.c
@@ -1,16 +1,17 @@
 #ifndef READER_WRITER_C
 #define READER_WRITER_C
 
-#define FILENAME "output.txt"
-
 #define mutex_init pthread_mutex_init
 #define mutex_lock pthread_mutex_lock
 #define mutex_unlock pthread_mutex_unlock
+#define mutex_destroy pthread_mutex_destroy
 
 #define semaphore_init sem_init
 #define semaphore_signal sem_post
-#define sempahore_wait sem_wait
+#define semaphore_wait sem_wait
+#define semaphore_destroy sem_destroy
 
+#define FILENAME "output.txt"
 #include "helper_functions.c"
 
 typedef pthread_mutex_t mutex;
@@ -18,12 +19,19 @@ typedef sem_t semaphore;
 
 /* locking mechanisms */
 static mutex            readerCountMutex;
-static semaphore        noWriters;
+static mutex            writerCountMutex;
+
+static semaphore        writerBlock;
+static semaphore        readerBlock;
+static semaphore        readerSerializer;
+
+/* accounting variables for locking mechanisms */
+static int              writerCount;
+static int              readerCount;
 
 /* accounting varibles */
-static int shouldContinue;
-static int readerCount;
-static int lineCount;
+static int              shouldContinue;
+static int              lineCount;
 
 /* buffer variable */
 static FILE*            buffer;
@@ -37,18 +45,29 @@ void init() {
 
   /* mutex lock setup */
   mutex_init(&readerCountMutex, NULL);
+  mutex_init(&writerCountMutex, NULL);
 
-  /* accounting integers setup */
-  shouldContinue = 1;
+  /* accounting variables for locking mechanisms setup */
   lineCount = 0;
   readerCount = 0;
 
+  /* accounting integers setup */
+  shouldContinue = 1;
+  writerCount = 0;
+
   /* semaphore setup */
-  semaphore_init(&noWriters, 0, 1);
+  semaphore_init(&readerSerializer, 0, 1);
+  semaphore_init(&writerBlock, 0, 1);
+  semaphore_init(&readerBlock, 0, 1);
 }
 
 /* de initialization code */
 void deInit() {
+  mutex_destroy(&readerCountMutex); /* kill all the things */
+  mutex_destroy(&writerCountMutex);
+  semaphore_destroy(&readerSerializer);
+  semaphore_destroy(&writerBlock);
+  semaphore_destroy(&readerBlock);
   fclose(buffer);
 }
 
@@ -59,31 +78,49 @@ void signalEnd() {
 
 static int writeItem(int item) {
 
-  sempahore_wait(&noWriters); /* get write lock */
+  /* if first writer, then prevent readers from entering queue */
+  mutex_lock(&writerCountMutex);
+  if(++writerCount == 1) {
+    semaphore_wait(&readerBlock);
+  }
+  mutex_unlock(&writerCountMutex);
+
+  semaphore_wait(&writerBlock); /* mutual exclusion over writing */
 
   /* write begins here*/
   fseek(buffer, 0, SEEK_END);
   fprintf(buffer, "%d\n", item);
   fflush(buffer);
 
-  printf("Writer: [ %2d ] %11d\n", lineCount, item);
+  printf("Writer: [ %4d ] %11d\n", lineCount + 1, item);
 
   lineCount++;
   /* write ends here */
 
-  semaphore_signal(&noWriters); /* release write lock */
+  semaphore_signal(&writerBlock);
+
+  mutex_lock(&writerCountMutex); /* if last writer allow readers to enter queue */
+  if(--writerCount == 0) {
+    semaphore_signal(&readerBlock);
+  }
+  mutex_unlock(&writerCountMutex);
 
   return 0;
 }
 
 static int readItem() {
 
-  mutex_lock(&readerCountMutex);
-  readerCount++;
-  if(readerCount == 1) {
-    sempahore_wait(&noWriters);
+  semaphore_wait(&readerSerializer); /* 1 reader at a time from here */
+  semaphore_wait(&readerBlock); /* attempt to enter queue */
+
+  mutex_lock(&readerCountMutex); /* if first reader then wait until writers are done then block */
+  if(++readerCount == 1) {
+    semaphore_wait(&writerBlock);
   }
   mutex_unlock(&readerCountMutex);
+
+  semaphore_signal(&readerBlock); /* if we have reader block, let reader into queue or writers to write */
+  semaphore_signal(&readerSerializer); /* let 1 reader into queueing section */
 
   /* read begins here */
   FILE* localBuffer = fopen(FILENAME, "r+");
@@ -103,14 +140,14 @@ static int readItem() {
 
   fclose(localBuffer);
 
-  printf("Reader: [ %2d ] %11d\n", lineToRead, val);
+  printf("Reader: [ %4d ] %11d\n", lineToRead + 1, val);
 
   /* read ends here */
 
+  /* if last reader let writers write */
   mutex_lock(&readerCountMutex);
-  readerCount--;
-  if(readerCount == 0) {
-    semaphore_signal(&noWriters);
+  if(--readerCount == 0) {
+    semaphore_signal(&writerBlock);
   }
   mutex_unlock(&readerCountMutex);
 
